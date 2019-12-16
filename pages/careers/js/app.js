b23e4ef82c8ca68f7c45a2c206a5a03815c75563
Felix <53485426+automalix@users.noreply.github.com>
2019-12-16 10:37:09 -0600

    Add files via upload

---
+++ pages/careers/js/app.js
@@ -0,0 +1,37 @@
+import { jObject } from './descriptions.js'
+
+(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})();
+
+document.querySelectorAll('[data-type^="job"]').forEach(cur => {
+    cur.addEventListener('click', event => {
+        // clear the innerHTML before loading the new content
+        [ document.querySelector('#jobDescTitle'),
+          document.querySelector('#job-desc'),
+          document.querySelector('#job-res'),
+          document.querySelector('#job-res-list'),
+          document.querySelector('#job-reqs'),
+          document.querySelector('#job-reqs-list') ].forEach(cur => cur.innerHTML = '');
+
+        const job = event.target.dataset.type;
+        document.querySelector('#jobDescTitle').innerHTML = jObject[job][0];
+
+        document.querySelector('#job-desc').innerHTML = jObject[job][1];
+
+        document.querySelector('#job-res').innerHTML = jObject[job][2];
+
+        let list = ['<li class="list-group-item">', '</li>']
+        for(let el of jObject[job][3]) {
+            el = list[0] + el + list[1];
+            document.querySelector('#job-res-list').innerHTML += el;
+        }
+
+        document.querySelector('#job-reqs').innerHTML = jObject[job][4];
+        for(let el of jObject[job][5]) {
+            el = list[0] + el + list[1];
+            document.querySelector('#job-reqs-list').innerHTML += el;
+        }
+    });
+})
+
---
+++ pages/careers/js/descriptions.js
@@ -0,0 +1,278 @@
+export const jObject = {
+    job1: [
+        `IT Project Manager`,
+
+        `IT Project Manager is responsible for the administration of network and server
+    infrastructure within all departments, planning of technology projects, managing our team of IT professionals, implementing technology security and the execution of all technology-related tasks and initiatives.`,
+
+        `IT Project Manager Responsibilities`,
+        [
+            `Provide technology strategy planning`,
+            `Integrate information technologies into the company for optimal effectiveness`,
+            `Understand current and future business goals and ongoing IT issues to ensure business success`,
+            `IT Project Manager Skills and Qualifications`,
+            `Bachelor’s Degree in Computer Science, Information Technology or related field`,
+            `CompTIA Advanced Security Practitioner (CASP) certification required`,
+            `6+ years’ experience as an IT professional`,
+            `2+ years’ supervisory experience in an IT Department`,
+            `In-depth knowledge of VMWare, Microsoft Windows, Google Apps administration, network administration and service desk administration`,
+            `Excellent written and verbal communication skills`,
+            `Able to clearly communicate technical concepts to both technical and non-technical audiences`,
+        ],
+
+        `IT Project Manager Skills and Qualifications`,
+        [
+            `Bachelor’s Degree in Computer Science, Information Technology or related field`,
+            `CompTIA Advanced Security Practitioner (CASP) certification required`,
+            `6+ years’ experience as an IT professional`,
+            `2+ years’ supervisory experience in an IT Department`,
+            `In-depth knowledge of VMWare, Microsoft Windows, Google Apps administration, network administration and service desk administration`,
+            `Excellent written and verbal communication skills`,
+            `Able to clearly communicate technical concepts to both technical and non-technical audiences`,
+        ]
+
+    ],
+    job2: [
+        `Senior Web Developer`,
+
+        `We are seeking a senior web developer capable of communicating with clients to determine website specifications, writing code, working with a team of designers and content writers, as well as assisting with recruitment and training of all new junior web developers. The Senior Web Developer will be responsible for updating our current online applications, as well as developing and implementing a usability testing process to insure that new website applications meet our company’s requirements.`,
+
+        `Senior Web Developer Responsibilities`,
+        [
+            `Create applications that address the phases of SDLC (software development life cycle)`,
+            `Develop comprehensive application testing procedures`,
+            `Update existing applications to meet the security and functionality standards as outlined in the company’s website policies`,
+            `Implement testing tools that monitor the ongoing performance of the company website`,
+            `Assist in updating application development policies to ensure that all future applications meet the latest technical requirements`,
+        ],
+
+        `Senior Web Developer Skills and Qualifications`,
+        [
+            `Bachelor’s Degree in Computer Science or related field (Master’s preferred)`,
+            `2+ years’ experience in application development and testing`,
+            `1+ years’ SDLC implementation experience`,
+            `Strong teamwork skills`,
+            `Proven knowledge of the most current security and web development programming languages`,
+        ],
+    ],
+    job3: [
+        `Junior Web Developer`,
+
+        `As a Junior Web Developer you will spend most a significant portion of your time coding and documenting. You will be assigned an engineering team responsible for one or more web applications, or features within a web application, and will design, test, code, deploy, iterate, and gather feedback from users.`,
+
+        `Junior Web Developer Responsibilities`,
+        [
+            `Be willing to be placed into an already establish team of coders`,
+            `Help design, build and implement new web applications`,
+            `Assist in the development of several already in-development, or completely new projects`,
+            `Be able to follow guidance from Senior Web Developers`,
+        ],
+
+        `Junior Web Developer Skills and Qualifications`,
+        [
+            `Understanding of relational databases; e.g., SQL, database administration, and complex queries.`,
+            `A familiarity with HTML, CSS, and JavaScript — knowledge of common frameworks is a bonus.`,
+            `Ability to adapt with changing technologies & frameworks, e.g., LESS/SASS, jQuery/Ember.js.`,
+            `Ability to learn new skills, tools, development kits, frameworks, etc.`,
+            `Appreciation for well-documented code and website / web service security.`,
+            `Previous experience building mid-size applications using HTML5 technologies.`,
+        ],
+    ],
+    job4: [
+        `Graphic Designer`,
+
+        `The Graphic designer creates visual concepts, by hand or using computer software, to communicate ideas that inspire, inform, or captivate consumers. They develop the overall layout and production design for advertisements, brochures, magazines, and corporate reports. They also have to consider things like negative space, readability and typography.`,
+
+        `Graphic Designer Responsibilities`,
+        [
+            `Work with other creative team members to produce content`,
+            `Create guidelines for how logos and other branding materials should be displayed and used`,
+            `Help make design choices (e.g., fonts and colors) for all content your company will use`,
+        ],
+
+        `Graphic Designer Skills and Qualifications`,
+        [
+            `Mastery of industry-standard software to create logos, design reports and complete other projects`,
+            `A compelling portfolio with projects showcasing skills that are relevant to your business's visual content goals.`,
+            `The ability to clearly communicate the reasoning behind design choices`,
+        ],
+    ],
+    
+    job5: [
+        `Senior Account Manager`,
+
+        `We’re looking for a self-starting professional with managerial experience and a proven track record in sales. The ideal candidate will look forward to coming to work each day and helping our customers find the applications that work best for them.`,
+
+        `Senior Account Manager Responsibilities`,
+        [
+            `Motivate and energize the staff to make proactive decisions in all their communications with clients`,
+            `Create and advertise weekly, monthly and quarterly sales goals and keep C-level executives apprised of the progress`,
+            `Prepare sales reports and sign off on their accuracy before submitting them`,
+            `Assess the competitive landscape and institute changes to the sales program in response to competitors’ changes`,
+            `Establish a referral network with local businesses and find ways to maximize value from those connections`,
+            `Implement a sales training and orientation program to educate new hires as our company grows`,
+        ],
+
+        `Senior Account Manager Skills and Qualifications`,
+        [
+            `Bachelor’s Degree in Marketing, Communications or Business Administration required (MBA preferred)`,
+            `7+ years experience in sales management`,
+            `10+ years experience in direct or outside sales (B2B a plus)`,
+            `Strong prospecting skills and excellent closing skills`,
+            `Exceptional verbal and written communication skills`,
+        ]
+    ],
+
+    job6: [
+        `Marketing Manager`,
+
+        `Cohort 11 Dev is seeking a determined and friendly professional with an advanced understanding of human behavior and a desire to help consumers find exactly the right product or service to suit their needs. Internet marketing managers are responsible for creating and implementing online marketing strategies to increase sales and revenue for a company. They maximize the technical and creative opportunities afforded by the breadth of Web-based static and social marketing in order to attract customers to the brand. The ideal candidate for this position has a positive attitude, a tireless work ethic and an insatiable appetite for growth.`,
+
+        `Marketing Manager Responsibilities`,
+        [
+            `Organize promotional events at trade shows and other venues, coordinating with our staff to ensure prompt delivery of materials and merchandise`,
+            `Hire and train a team of marketing associates to coordinate events and conduct outside sales`,
+            `Identify weaknesses in existing marketing campaigns and develop pragmatic solutions within budgetary constraints`,
+            `Communicate with senior management about marketing initiatives and project metrics, as well as to brainstorm fresh strategies`,
+        ],
+
+        `Marketing Manager Skills and Qualifications`,
+        [
+            `Bachelor’s Degree in Marketing, Business Administration, Communications or related field (MBA preferred)`,
+            `10+ years in marketing and promotion`,
+            `Ability to work unpredictable hours, including some evenings and weekends`,
+            `Excellent communication and decision-making skills`,
+            `Familiarity with online content marketing and social media development strategies`,
+            `Proficiency with HubSpot marketing suite and SalesFUSION360 software`,
+        ]
+    ],
+
+    job7: [
+        `Customer Service Manager`,
+
+        `Our organization’s Customer Service Manager focuses on all aspects of customer satisfaction and care as well as the supervision of our dedicated Customer Service Associates. As a Customer Service Manager, your most important role is leading and motivating a team of staff to ensure they’re giving the best customer service possible. You’ll also be expected to know our company’s products and policies inside out and be able to communicate it to other members of staff.`,
+
+        `Customer Service Manager Responsibilities`,
+        [
+            `Oversee our customer service team`,
+            `Recruit, manage and train Customer Associates`,
+            `Set reasonable customer satisfaction goals and work with the team to meet them on a consistent basis`,
+            `Interact with customers on a daily basis, responding to complaints and queries (from customers and staff) and guiding them to the appropriate service`,
+            `On-board new employees and train them based on your expertise and skill set`,
+            `Create and implement an effective customer loyalty program`,
+        ],
+
+        `Customer Service Manager Skills And Qualifications`,
+        [
+            `10+ years’ experience in customer service`,
+            `5+ years’ supervisory experience`,
+            `Bachelor’s Degree in Business Administration or a related field preferred`,
+            `Extensive e-commerce background`,
+            `Ability to translate your skills to other employees through training and mentoring`,
+            `Excellent verbal and written communication skills`,
+            `Proficiency with Microsoft Office Suite and Google Docs`,
+        ],
+
+    ],
+
+    job8: [
+        `Back-End Web Developer`,
+
+        `Back-End Web Developer is responsible for managing the interchange of data between the server and the users. Your primary focus will be development of all server-side logic, definition and maintenance of the central database, and ensuring high performance and responsiveness to requests from the front-end. You will also be responsible for integrating the front-end elements built by your coworkers into the application`,
+
+        `Back-End Web Developer Responsibilities`,
+        [
+        `Integration of user-facing elements developed by a front-end developers with server side logic`,
+        `Building reusable code and libraries for future use`,
+        `Optimization of the application for maximum speed and scalability`,
+        `Implementation of security and data protection`,
+        `Design and implementation of data storage solutions`,
+        ],
+
+        `Back-End Web Developer Skills And Qualifications`,
+        [
+        	`Basic understanding of front-end technologies and platforms, such as JavaScript, HTML5, and CSS3`,
+        	`Good understanding of server-side CSS preprocessors, such as LESS and SASS`,
+        	`Understanding accessibility and security compliance`,
+            `User authentication and authorization between multiple systems, servers, and environments`,
+            `Integration of multiple data sources and databases into one system`,
+        	`Management of hosting environment, including database administration and scaling an application to support load changes`,
+        	`Data migration, transformation, and scripting`,
+        	`Outputting data in different formats`,
+        	`Understanding differences between multiple delivery platforms such as mobile vs desktop, and optimizing output to match the specific platform`,
+        	`Implementing automated testing platforms and unit tests`,
+            `Proficient knowledge of a back-end programming language.`,
+            `Depending on the specific case, a developer should have the knowledge of one or more of PHP, Python, Ruby, Java, .NET, JavaScript`,
+        	`Proficient understanding of code versioning tools, such as Git`,
+        	`Proficient understanding of OWASP security principles`,
+        	`Understanding of “session management” in a distributed server environment`,
+        ],
+    ],
+
+    job9: [
+        `Social Media Coordinator`,
+
+        `Social Media Coordinator is responsible for planning, implementing and monitoring a company's Social Media strategy in order to increase brand awareness, improve Marketing efforts and increase sales.<br>
+        We are looking for an experienced and creative Social Media Coordinator to join our marketing team.
+        As a Social Media Coordinator, you will be responsible for developing and implementing our Social Media strategy in order to increase our online presence and improve our marketing and sales efforts.<br>
+        You will be working closely with the Marketing and Sales departments.`,
+
+        `Social Media Coordinator Responsibilities`,
+        [
+            `Develop, implement and manage our social media strategy`,
+            `Work with developers to ensure content is informative and appealing`,
+            `Collaborate with Marketing, Sales and Product Development teams`,
+            `Manage and oversee social media content`,
+            `Measure the success of every social media campaign`,
+            `Keep abreast of the latest social media best practices and technologies`,
+            `Use social media marketing tools such as Buffer`,
+            `Monitor SEO and user engagement and suggest content optimization`,
+            `Communicate with industry professionals and influencers via social media to create a strong network`,
+        ],
+
+        `Social Media Coordinator Skills And Qualifications`,
+        [
+            `2 years of experience as a Social Media Coordinator or similar role`,
+            `Excellent knowledge of Facebook, Twitter, LinkedIn, Pinterest, Instagram, Google+ and other social media best practices`,
+            `Understanding of SEO and web traffic metrics`,
+            `Experience with doing audience and buyer persona research`,
+            `Good understanding of social media KPIs`,
+            `Familiarity with web design and publishing`,
+            `Excellent multitasking skills`,
+            `BA in Marketing or similar relevant field a plus`,
+            `Great interpersonal and communication skills`,
+            `Critical thinker and problem-solving skills`,
+        ],
+    ],
+
+    job10: [
+        `Creative Content Director`,
+
+        `Our growing company is in need of an experienced and ambitious Creative Content Director to oversee development, execution and delivery of digital and print content across all our channels. You will play a key role in the success of our inbound and outbound marketing campaigns.<br>
+        The ideal candidate should know how to apply data analytics to personalize and optimize content. The Creative Content Director will manage an expanding team of Content Strategists, interact with C-level team members and build new opportunities across our client engagements. The Content Director will be a solid leader, clear communicator and possess great analytical skills. Candidates must be detail oriented, proactive, organized and have the ability to follow-up often and in a friendly and courteous manner.`,
+
+        `Creative Content Director Responsibilities`,
+
+        [
+            `Plan and execute a content strategy that drives high engagement`,
+            `Serve as editor-in-chief for all content to ensure quality, clarity and consistency`,
+            `Collect, monitor and analyze social and website metrics, and make improvements accordingly`,
+            `Stay updated on industry trends and adjust the content strategy as needed`,
+            `Collaborate with brand, product and sales managers to create promotional concepts`,
+            `Manage the distribution of content across all channels`,
+        ],
+
+        `Creative Content Director Skills And Qualifications`,
+
+        [
+            `Bachelor’s Degree preferred`,
+            `Understanding of content marketing tactics, such as SEO, digital advertising and social media marketing`,
+            `Demonstrated ability to lead content marketing campaigns`,
+            `Strong Google Analytics, WordPress and Adobe Acrobat/Illustrator skills`,
+            `Excellent written and verbal communication skills`,
+            `2+ years of experience in Content Marketing`,
+        ],
+    ],
+
+    
+}
