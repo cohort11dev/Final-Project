1ab83ca801dea62bafb6f601b6a5ad8c7b5c15eb
Chelsea Griffith <chelseaegriffith@gmail.com>
2019-12-16 12:59:33 -0600

    add hover functionality

---
+++ MeetOurTeam/js/teamMembers.js
@@ -0,0 +1,80 @@
+const teamMembers = [
+    {
+        name: "Chelsea Griffith",
+        title: "Senior Web Developer",
+        bio: "lorem ipsum",
+        image: "images/Chelsea.jpg"
+    },
+    {
+        name: "Saodie Chhan",
+        title: "Customer Service Manager",
+        bio: "lorem ipsum",
+        image: "images/Saodie.jpg"
+    },
+    {
+        name: "Christopher Walls",
+        title: "Graphic Designer",
+        bio: "lorem ipsum",
+        image: "images/Walls.jpg"
+    },
+    {
+        name: "Rosalind Balton",
+        title: "IT Project Manager",
+        bio: "lorem ipsum",
+        image: "images/Rosalind.jpg"
+    },
+    {
+        name: "Felix Gelly",
+        title: "Senior Web Developer",
+        bio: "lorem ipsum",
+        image: "images/Felix.png"
+    },
+    {
+        name: "Steve McNatt",
+        title: "Senior Web Developer",
+        bio: "lorem ipsum",
+        image: "images/Steve.jpg"
+    },
+    {
+        name: "Christopher Appleton",
+        title: "Marketing Manager",
+        bio: "lorem ipsum",
+        image: "images/Appleton.jpg"
+    },
+    {
+        name: "Katie Kutz",
+        title: "Junior Web Developer",
+        bio: "lorem ipsum",
+        image: "images/Katie.jpg"
+    },
+    {
+        name: "Donniesha Gregory",
+        title: "Senior Account Manager",
+        bio: "lorem ipsum",
+        image: "images/Donn.jpg"
+    },
+    {
+        name: "Dominique Chhan",
+        title: "Social Media Coordinator",
+        bio: "lorem ipsum",
+        image: "images/Dominique.jpg"
+    },
+    {
+        name: "Anthony Romano",
+        title: "Back-End Web Developer",
+        bio: "lorem ipsum",
+        image: "images/Romano.jpg"
+    },
+    {
+        name: "Andrea Kraemer",
+        title: "Creative Content Director",
+        bio: "lorem ipsum",
+        image: "images/Andrea.jpg"
+    },
+    {
+        name: "Scarlett Liu",
+        title: "Junior Web Developer",
+        bio: "lorem ipsum",
+        image: ""
+    }
+]
