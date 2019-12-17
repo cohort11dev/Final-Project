771586e49ec9bd4e6bf07bfb58aba49d1ec5d1ff
Felix <53485426+automalix@users.noreply.github.com>
2019-12-17 15:44:14 -0600

    Update jumbotron.js

--- assets/nav/jumbotron.js
+++ assets/nav/jumbotron.js
@@ -3,7 +3,7 @@ export const jumbotron = [
         page: `about`,
         header1: `learn about`,
         header2: `cohort 11`,
-        text: ``
+        text: `In September of 2019, a diverse group of individuals descended upon the YWEB Career Academy serendipitously, unaware of the significance it would have on their lives. A rare opportunity to aspire to great heights, broaden horizons presented itself by studying front end web development at the YWCA.`
     },
 
     {
@@ -20,14 +20,14 @@ export const jumbotron = [
         page: `blog`,
         header1: `insight`,
         header2: `our blog`,
-        text: ``
+        text: `A peek into the minds of various team members. On this page you'll find development tips and tricks, lessons learned, and much more.`
     },
 
     {
         page: `contact`,
         header1: `reach out`,
         header2: `contact us`,
-        text: ``
+        text: `We are always available via email for any inquiries.<br> We are also available for private consultations.`
     },
 
     {
@@ -41,6 +41,6 @@ export const jumbotron = [
         page: `ourteam`,
         header1: `meet our team`,
         header2: `cohort 11`,
-        text: `Learn about the individual members of the Cohort 11 Dev team!`
+        text: `Learn about the individual members of Cohort 11 Dev! Our team is passionate about designing exceptional websites and helping the community develop their digital footprint in the world.`
     },
 ]
