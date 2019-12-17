69d8eca5cb953467263317d139577f0e85631ca8
Felix <53485426+automalix@users.noreply.github.com>
2019-12-16 23:24:51 -0600

    for card generation

---
+++ pages/careers/js/cards.js
@@ -0,0 +1,52 @@
+export const card = [
+    {
+    title: `IT Project Manager`,
+    description: `IT Project Manager is responsible for the administration of network and server
+    infrastructure within all departments, planning of technology projects, managing our team of IT professionals, implementing technology security and the execution of all technology-related tasks and initiatives.`,
+    },
+
+    {
+    title: `Senior Web Developer`,
+    description: `Senior Web Developer's responsibilities include communicating with clients to determine website specifications, writing code, working with a team of designers and content writers, as well as assisting with recruitment and training of all new junior web developers.`,
+    },
+
+    {
+    title: `Junior Web Developer`,
+    description: `As a Junior Web Developer you will spend a significant portion of your time coding and documenting.`,
+    },
+
+    {
+    title: `Graphic Designer`,
+    description: `The Graphic designer creates visual concepts, by hand or using computer software, to communicate ideas that inspire, inform, or captivate consumers.`,
+    },
+
+    {
+    title: `Senior Account Manager`,
+    description: `We’re looking for a self-starting professional with managerial experience and a proven track record in sales. The ideal candidate will look forward to coming to work each day and helping our customers find the applications that work best for them.`,
+    },
+
+    {
+    title: `Marketing Manager`,
+    description: `Internet marketing managers are responsible for creating and implementing online marketing strategies to increase sales and revenue for a company.`,
+    },
+
+    {
+    title: `Customer Service Manager`,
+    description: `Our organization’s Customer Service Manager focuses on all aspects of customer satisfaction and care as well as the supervision of our dedicated Customer Service Associates.`,
+    },
+
+    {
+    title: `Back-End Web Developer`,
+    description: `Back-End Web Developer is responsible for managing the interchange of data between the server and the users.`,
+    },
+
+    {
+    title: `Social Media Coordinator`,
+    description: `Social Media Coordinator is responsible for planning, implementing and monitoring a company's Social Media strategy in order to increase brand awareness, improve Marketing efforts and increase sales.`,
+    },
+    
+    {
+    title: `Creative Content Director`,
+    description: `This role will direct the Creative Content Team creative content execution across multiple lines of business and departments; including but not limited to theatrical marketing EPK and publicity content, digital/social content, specialty broadcast content.`,
+    },
+]
