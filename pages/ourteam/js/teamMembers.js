0a99176404d369a0509efc34ac62e9456186d4ef
Felix <53485426+automalix@users.noreply.github.com>
2019-12-17 15:12:33 -0600

    Update teamMembers.js

--- pages/ourteam/js/teamMembers.js
+++ pages/ourteam/js/teamMembers.js
@@ -2,79 +2,79 @@ export const teamMembers = [
     {
         name: "Chelsea Griffith",
         title: "Senior Web Developer",
-        bio: "lorem ipsum",
-        image: "./ourteam/images/Chelsea.jpg"
+        bio: "Baker, Traveler, Pizza enthusiast",
+        image: "../pages/ourteam/images/Chelsea.jpg"
     },
     {
         name: "Saodie Chhan",
         title: "Customer Service Manager",
-        bio: "lorem ipsum",
-        image: "./ourteam/images/Saodie.jpg"
+        bio: "Marvel lover. My favorite color is forest green.",
+        image: "../pages/ourteam/images/Saodie.jpg"
     },
     {
         name: "Christopher Walls",
         title: "Graphic Designer",
-        bio: "lorem ipsum",
-        image: "./ourteam/images/Walls.jpg"
+        bio: "BAMF with a passion to improve daily. Learned to code to build skills. Graphic designer building off of the world around me.",
+        image: "../pages/ourteam/images/Walls.jpg"
     },
     {
         name: "Rosalind Balton",
         title: "IT Project Manager",
-        bio: "lorem ipsum",
-        image: "./ourteam/images/Rosalind.jpg"
+        bio: "Came to code for BIG$, stayed in code camp for the challenge. Favorite cartoon character is Foghorn Leghorn.",
+        image: "../pages/ourteam/images/Rosalind.jpg"
     },
     {
         name: "Felix Gelly",
         title: "Senior Web Developer",
-        bio: "lorem ipsum",
-        image: "./ourteam/images/Felix.png"
+        bio: "I own two rabbits. Favourite colour is chartreuse.",
+        image: "../pages/ourteam/images/Felix.png"
     },
     {
         name: "Steve McNatt",
         title: "Senior Web Developer",
-        bio: "lorem ipsum",
-        image: "./ourteam/images/Steve.jpg"
+        bio: "I chose to code because I enjoy solving problems. Favorite cartoon character is Marvin Martian.",
+        image: "../pages/ourteam/images/Steve.jpg"
     },
     {
         name: "Christopher Appleton",
         title: "Marketing Manager",
-        bio: "lorem ipsum",
-        image: "./ourteam/images/Appleton.jpg"
+        bio: "Essex saved Azur Lane.",
+        image: "../pages/ourteam/images/Appleton.jpg"
     },
     {
         name: "Katie Kutz",
         title: "Junior Web Developer",
-        bio: "lorem ipsum",
-        image: "./ourteam/images/Katie.jpg"
+        bio: "I love playing classical piano, rooting for the Green Bay Packers and volunteering in my community.",
+        image: "../pages/ourteam/images/Katie.jpg"
     },
     {
         name: "Donniesha Gregory",
         title: "Senior Account Manager",
-        bio: "lorem ipsum",
-        image: "./ourteam/images/Donn.jpg"
+        bio: "Life has no time constraints, get out of the fast lane and enjoy life. It does not matter when you arrive, as long as you are happy when you get there.",
+        image: "../pages/ourteam/images/Donn.jpg"
     },
     {
         name: "Dominique Chhan",
         title: "Social Media Coordinator",
-        bio: "lorem ipsum",
-        image: "./ourteam/images/Dominique.jpg"
+        bio: "Animal lover. Food and boba enthusiast.",
+        image: "../pages/ourteam/images/Dominique.jpg"
     },
     {
         name: "Anthony Romano",
         title: "Back-End Web Developer",
-        bio: "lorem ipsum",
-        image: "./ourteam/images/Romano.jpg"
+        bio: "I started coding because of the movie Tron. I love spending time with my wife and boys. and I love writing short science fiction stories.",
+        image: "../pages/ourteam/images/Romano.jpg"
     },
     {
         name: "Andrea Kraemer",
         title: "Creative Content Director",
-        bio: "lorem ipsum",
-        image: "./ourteam/images/Andrea.jpg"
+        bio: "I found coding or..coding found me? Fresh out of the oven, web developer. Nature enthusiast, treasure hunter, photographer, & Monarch mama.",
+        image: "../pages/ourteam/images/Andrea.jpg"
     },
     {
         name: "Scarlett Liu",
         title: "Junior Web Developer",
-        bio: "lorem ipsum",
-        image: ""
+        bio: "I love to garden with my kids and my favorite color is green.",
+        image: "../pages/ourteam/images/Scarlett.jpg"
     }
 ]
