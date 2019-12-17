45a86c31cc36f9442937909754a8b1ab56d607d6
Felix <53485426+automalix@users.noreply.github.com>
2019-12-17 09:42:26 -0600

    Update cards.js

--- pages/portfolio/js/cards.js
+++ pages/portfolio/js/cards.js
@@ -11,7 +11,7 @@ export const portfolioCards = [
         demoImage: `./portfolio/portfolio/cgriffith/demo.jpg`,
         name: `Chelsea Griffith`,
         title: `Fiddle and Pipes`,
-        description: `A short description of the website.`,
+        description: `A single page restaurant demo with strong image showcasing and JavaScript functionality.`,
         link: `https://github.com/cohort11dev/Final-Project/tree/demo/pages/portfolio/portfolio/cgriffith`,
     },
 
@@ -19,7 +19,7 @@ export const portfolioCards = [
         demoImage: `./portfolio/portfolio/tromano/demo.jpg`,
         name: `Anthony Romano`,
         title: `Swervin Vegan's`,
-        description: `A short description of the website.`,
+        description: `A single page, highly responsive restaurant demo highlighting several sections and utilizing animations.`,
         link: `https://github.com/cohort11dev/Final-Project/tree/demo/pages/portfolio/portfolio/tromano`,
     },
 
@@ -27,7 +27,7 @@ export const portfolioCards = [
         demoImage: `./portfolio/portfolio/smcnatt/demo.jpg`,
         name: `Steve McNatt`,
         title: `Steve's Bar and Grill`,
-        description: `A short description of the website.`,
+        description: `A restaurant demo with a static navbar header, footer, list of menu items with buttons to order items, and cart functionality.`,
         link: `https://github.com/cohort11dev/Final-Project/tree/demo/pages/portfolio/portfolio/smcnatt`,
     },
 
@@ -35,7 +35,7 @@ export const portfolioCards = [
         demoImage: `./portfolio/portfolio/cwalls/demo.jpg`,
         name: `Christopher Walls`,
         title: `Jerk Paradise`,
-        description: `A short description of the website.`,
+        description: `This restaurant demo site showcases the use of flex boxes and menu cards.`,
         link: `https://github.com/cohort11dev/Final-Project/tree/demo/pages/portfolio/portfolio/cwalls`,
     },
 
