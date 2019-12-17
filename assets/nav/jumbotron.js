bf4644359ce44c1f0c331e48b869c7c86e623840
Felix <53485426+automalix@users.noreply.github.com>
2019-12-17 13:15:31 -0600

    Create jumbotron.js

---
+++ assets/nav/jumbotron.js
@@ -0,0 +1,46 @@
+export const jumbotron = [
+    {
+        page: `about`,
+        header1: `learn about`,
+        header2: `cohort 11`,
+        text: ``
+    },
+
+    {
+        page: `portfolio`,
+        header1: `our work`,
+        header2: `portfolio`,
+        text: `Our dedicated team of designers are passionate about developing custom websites. Our goal is to create inspired web solutions that meet your exact needs and propel your business forward.<br><br>
+
+        Below is a brief preview of our restaurant-themed sites created by several members
+        <br>of the Cohort 11 Dev team of coders.`
+    },
+
+    {
+        page: `blog`,
+        header1: `insight`,
+        header2: `our blog`,
+        text: ``
+    },
+
+    {
+        page: `contact`,
+        header1: `reach out`,
+        header2: `contact us`,
+        text: ``
+    },
+
+    {
+        page: `careers`,
+        header1: `opportunities`,
+        header2: `careers`,
+        text: `Learn about various career opportunities open in Cohort 11. We are looking for hard working members of the community that share in our missions and values.`
+    },
+
+    {
+        page: `ourteam`,
+        header1: `meet our team`,
+        header2: `cohort 11`,
+        text: `Learn about the individual members of the Cohort 11 Dev team!`
+    },
+]
