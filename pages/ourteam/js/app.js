0e4a2965eb9bf7067c6be6a1f875706cedc0c903
Felix <53485426+automalix@users.noreply.github.com>
2019-12-16 21:14:19 -0600

    Thanks Chelsea!

--- pages/ourteam/js/app.js
+++ pages/ourteam/js/app.js
@@ -1,4 +1,24 @@
-for (let card of document.querySelectorAll(".card")) {
-    card.addEventListener("mouseenter", event => console.log(event));
+import {teamMembers} from './teamMembers.js'
+
+const renderTeamMember = (person) => {
+    let markup;
+    markup = `
+        <div class="col-6 col-md-4 py-1 text-center">
+            <div class="card border-0">
+                <img src="${person.image}" class="card-img-top" alt="${person.name}">
+                <div class="card-img-overlay">
+                    <p class="card-text text-center">${person.bio}</p>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">${person.name}</h5>
+                    <p class="card-text">${person.title}</p>
+                </div>
+            </div>
+        </div>
+    `;
+    document.querySelector(".row").insertAdjacentHTML("beforeend", markup);
 }
 
+for (let person of teamMembers) {
+    renderTeamMember(person);
+}
---
+++ pages/ourteam/js/teamMembers.js
@@ -0,0 +1,80 @@
+export const teamMembers = [
+    {
+        name: "Chelsea Griffith",
+        title: "Senior Web Developer",
+        bio: "lorem ipsum",
+        image: "./ourteam/images/Chelsea.jpg"
+    },
+    {
+        name: "Saodie Chhan",
+        title: "Customer Service Manager",
+        bio: "lorem ipsum",
+        image: "./ourteam/images/Saodie.jpg"
+    },
+    {
+        name: "Christopher Walls",
+        title: "Graphic Designer",
+        bio: "lorem ipsum",
+        image: "./ourteam/images/Walls.jpg"
+    },
+    {
+        name: "Rosalind Balton",
+        title: "IT Project Manager",
+        bio: "lorem ipsum",
+        image: "./ourteam/images/Rosalind.jpg"
+    },
+    {
+        name: "Felix Gelly",
+        title: "Senior Web Developer",
+        bio: "lorem ipsum",
+        image: "./ourteam/images/Felix.png"
+    },
+    {
+        name: "Steve McNatt",
+        title: "Senior Web Developer",
+        bio: "lorem ipsum",
+        image: "./ourteam/images/Steve.jpg"
+    },
+    {
+        name: "Christopher Appleton",
+        title: "Marketing Manager",
+        bio: "lorem ipsum",
+        image: "./ourteam/images/Appleton.jpg"
+    },
+    {
+        name: "Katie Kutz",
+        title: "Junior Web Developer",
+        bio: "lorem ipsum",
+        image: "./ourteam/images/Katie.jpg"
+    },
+    {
+        name: "Donniesha Gregory",
+        title: "Senior Account Manager",
+        bio: "lorem ipsum",
+        image: "./ourteam/images/Donn.jpg"
+    },
+    {
+        name: "Dominique Chhan",
+        title: "Social Media Coordinator",
+        bio: "lorem ipsum",
+        image: "./ourteam/images/Dominique.jpg"
+    },
+    {
+        name: "Anthony Romano",
+        title: "Back-End Web Developer",
+        bio: "lorem ipsum",
+        image: "./ourteam/images/Romano.jpg"
+    },
+    {
+        name: "Andrea Kraemer",
+        title: "Creative Content Director",
+        bio: "lorem ipsum",
+        image: "./ourteam/images/Andrea.jpg"
+    },
+    {
+        name: "Scarlett Liu",
+        title: "Junior Web Developer",
+        bio: "lorem ipsum",
+        image: ""
+    }
+]
