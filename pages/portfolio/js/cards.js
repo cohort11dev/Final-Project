323d8a7af3593a3e68efbd3ea563da94d7824934
Felix <53485426+automalix@users.noreply.github.com>
2019-12-17 00:22:43 -0600

    todo descriptions

---
+++ pages/portfolio/js/cards.js
@@ -0,0 +1,49 @@
+export const portfolioCards = [
+    {
+        demoImage: `./portfolio/portfolio/fgelly/demo.jpg`,
+        name: `Felix Gelly`,
+        title: `The Lunch Box`,
+        description: `A short restaurant demo showcasing dynamically created JavaScript Elements.`,
+        link: `https://github.com/cohort11dev/Final-Project/tree/demo/pages/portfolio/portfolio/fgelly`,
+    },
+
+    {
+        demoImage: `./portfolio/portfolio/cgriffith/demo.jpg`,
+        name: `Chelsea Griffith`,
+        title: `Fiddle and Pipes`,
+        description: `A short description of the website.`,
+        link: `https://github.com/cohort11dev/Final-Project/tree/demo/pages/portfolio/portfolio/cgriffith`,
+    },
+
+    {
+        demoImage: `./portfolio/portfolio/tromano/demo.jpg`,
+        name: `Anthony Romano`,
+        title: `Swervin Vegan's`,
+        description: `A short description of the website.`,
+        link: `https://github.com/cohort11dev/Final-Project/tree/demo/pages/portfolio/portfolio/tromano`,
+    },
+
+    {
+        demoImage: `./portfolio/portfolio/smcnatt/demo.jpg`,
+        name: `Steve McNatt`,
+        title: `Steve's Bar and Grill`,
+        description: `A short description of the website.`,
+        link: `https://github.com/cohort11dev/Final-Project/tree/demo/pages/portfolio/portfolio/smcnatt`,
+    },
+
+    {
+        demoImage: `./portfolio/portfolio/cwalls/demo.jpg`,
+        name: `Christopher Walls`,
+        title: `Jerk Paradise`,
+        description: `A short description of the website.`,
+        link: `https://github.com/cohort11dev/Final-Project/tree/demo/pages/portfolio/portfolio/cwalls`,
+    },
+
+    {
+        demoImage: `./portfolio/portfolio/cappleton/demo.jpg`,
+        name: `Christopher Applton`,
+        title: `Restaurant Demo`,
+        description: `A short description of the website.`,
+        link: `https://github.com/cohort11dev/Final-Project/tree/demo/pages/portfolio/portfolio/cappleton`,
+    },
+]
